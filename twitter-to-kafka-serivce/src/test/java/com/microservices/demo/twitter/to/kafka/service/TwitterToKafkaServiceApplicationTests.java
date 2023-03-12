7d07210d69852c51ba1dbc8e8ac1d6a6a4b50544
ta <pmzita@hotmail.com>
2023-03-12 20:19:10 +0700

    twitter init

---
+++ twitter-to-kafka-serivce/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+    @Test
+    public void contextLoad(){
+
+    }
+}
